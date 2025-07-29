b5c3ce6dda830a0e03d6abb9c69de1549773503f
Kristián Řehoř <kmancz.kiki@gmail.com>
2025-07-29 18:50:46 +0200

    Read input file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{env, process};
+use std::{env, process, fs::File, io::BufReader};
+use std::io::Read;
 
 enum ProgramError {
     WrongNumberOfArguments,
+    FileOpenError,
 }
 
 fn check_args(args: &[String]) -> Result<(), ProgramError> {
@@ -11,18 +13,45 @@ fn check_args(args: &[String]) -> Result<(), ProgramError> {
     Ok(())
 }
 
+fn prepare_file(path: &str) -> Result<BufReader<File>, ProgramError> {
+    let f = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => return Err(ProgramError::FileOpenError),
+    };
+
+    let reader = BufReader::new(f);
+
+    Ok(reader)
+}
+
 fn handle_errors(err: ProgramError) {
     match err {
         ProgramError::WrongNumberOfArguments => {
             eprintln!("Error: wrong number of arguments");
         }
+        ProgramError::FileOpenError => {
+            eprintln!("Error: failed to open file");
+        }
     }
-    
+
     process::exit(1);
 }
+
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     if let Err(err) = check_args(&args) {
         handle_errors(err);
     }
+
+    let mut f = match prepare_file(&args[0]) {
+        Ok(file) => file,
+        Err(err) => {
+            handle_errors(err);
+            return;
+        }
+    };
+
+    let mut buff = String::new();
+    let _ = f.read_to_string(&mut buff);
+    println!("{}", buff);
 }
