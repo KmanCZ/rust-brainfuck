16564120e83a92975e20dc1d18d6dfaaf6345203
Kristián Řehoř <kmancz.kiki@gmail.com>
2025-07-29 18:01:29 +0200

    First parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+#[derive(Debug)]
+enum ProgramError {
+    WrongNumberOfArguments,
+}
+
+fn check_args(args: &[String]) -> Result<(), ProgramError> {
+    if args.len() != 1 {
+        return Err(ProgramError::WrongNumberOfArguments);
+    }
+    Ok(())
+}
+fn main() -> Result<(), ProgramError> {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if let Err(err) = check_args(&args) {
+        return Err(err);
+    }
+    
+    Ok(())
 }
